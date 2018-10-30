af78da46b0972140c43e1b1ac3c885d0bd7a30b7
Casey Dale Spires <casey.spires@eku.edu>
2018-10-30 16:10:23 -0400

    Initialize

---
+++ API/Controllers/IdentityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
---
+++ Client/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Client.Models;
+using IdentityModel.Client;
+
+namespace Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        public async Task<IActionResult> RequestToken()
+        {
+            var discover = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (discover.IsError)
+            {
+                Console.WriteLine(discover.Error);
+            }
+            var tokenClient = new TokenClient(discover.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("API");
+            if(tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+            }
+            return View(tokenResponse);
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
